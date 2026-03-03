a0b7f0b18512bb4cd7c316eb4471c5b9c75027ca
julianollivieira <julian.ollivieira@gmail.com>
2026-03-03 21:04:48 +0100

    start over

--- src/db.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(feature = "sqlite")]
-pub type DatabaseConnection = diesel::SqliteConnection;
-#[cfg(feature = "postgres")]
-pub type DatabaseConnection = diesel::PgConnection;
-#[cfg(feature = "mysql")]
-pub type DatabaseConnection = diesel::MySqlConnection;
